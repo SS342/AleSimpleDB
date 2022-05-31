467cc39362a1f36066f04aabb2960c857d1ec130
Alexey Ovchinnikov <74982379+SS342@users.noreply.github.com>
2022-05-31 20:05:44 +0300

    Add files via upload

---
+++ v3/AleSimpleDB/get.py
@@ -0,0 +1,15 @@
+import sqlite3
+
+def get_all_writes(cursor : sqlite3.Cursor, tableName : str):
+    return cursor.execute(f"SELECT * FROM {tableName}").fetchall()
+
+
+def get_writes_by(cursor : sqlite3.Cursor, tableName : str, param: str, query : str): 
+    return cursor.execute(f"SELECT * FROM {tableName} WHERE {param} = '{query}'").fetchall()
+
+
+def get_last(cursor : sqlite3.Cursor, tableName : str):
+    return cursor.execute(f'SELECT * FROM {tableName}').fetchall()[-1]
+
+def get_last_by(cursor : sqlite3.Cursor, tableName : str, param: str, query : str):
+    return cursor.execute(f'SELECT * FROM {tableName} WHERE {param} = "{query}"').fetchall()[-1]
